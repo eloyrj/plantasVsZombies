23aceb32e1099cd6a3619589308839311df8c5de
chanchantal <chantalmaria103@gmail.com>
2019-04-11 21:37:23 +0200

    Academia
    no se si deberia escribir esto pero es lo que he hecho en la academia que lo disfrutes

---
+++ src/plantavszombies/Partida.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plantavszombies;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Partida {
+    private int dinero;
+    private int turno; //turno = ciclo
+    private String dificultad; // baja, media, alta, imposible
+   
+    
+    
+}
